c0cca3ae15d153696d644ee2cd5ee4aaef3fcf24
ankhly <91781956+ankhly@users.noreply.github.com>
2022-04-20 22:58:43 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -73,34 +73,8 @@ document.addEventListener('DOMContentLoaded', () => {
 //=====================
 
 
-//let _slideUp = (target, duration = 500) => {
-//	if (!target.classList.contains('_slide')) {
-//		target.classList.add('_slide');
-//		target.style.transitionProperty = 'height, margin, padding';
-//		target.style.transitionDuration = duration + 'ms';
-//		target.style.height = target.offsetHeight + 'px';
-//		target.offsetHeight;
-//		target.style.overflow = 'hidden';
-//		target.style.height = 0;
-//		target.style.paddingTop = 0;
-//		target.style.paddingBottom = 0;
-//		target.style.marginTop = 0;
-//		target.style.marginBottom = 0;
-//		window.setTimeout(() => {
-//			target.hidden = true;
-//			target.style.removeProperty('height');
-//			target.style.removeProperty('padding-top');
-//			target.style.removeProperty('padding-bottom');
-//			target.style.removeProperty('margin-top');
-//			target.style.removeProperty('margin-bottom');
-//			target.style.removeProperty('overflow');
-//			target.style.removeProperty('transition-duration');
-//			target.style.removeProperty('transition-property');
-//			target.classList.remove('_slide');
-//		}, duration);
-//	}
-//}
-let _slideDown = (target, duration = 500) => {
+
+let _slideDown = (target, duration = 300) => {
 	if (!target.classList.contains('_slide')) {
 		target.classList.add('_slide');
 		if (target.hidden) {
@@ -130,13 +104,6 @@ let _slideDown = (target, duration = 500) => {
 		}, duration);
 	}
 }
-//let _slideToggle = (target, duration = 500) => {
-//	if (target.hidden) {
-//		return _slideDown(target, duration);
-//	} else {
-//		return _slideUp(target, duration);
-//	}
-//}
 
 
 
@@ -147,7 +114,6 @@ if (iconMenu) {
 
 		iconMenu.classList.toggle('_active');
 		menuBody.classList.toggle('_active');
-
 
 		_slideDown(menuBody);
 	});
